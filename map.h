b89ba545d18267b7d375afc9bb31befb1538d437
Finnegan Thistle <113639635+FinnThistle@users.noreply.github.com>
2023-03-26 21:21:15 -0400

    Create map.h

---
+++ map.h
@@ -0,0 +1,43 @@
+// NOTE: This is not my map implementation, it was given to me to complete this assignment by my proffesor. All credit goes to him. The following code is all his.
+// map.h
+//
+// This is a basic map data structure for key, value pairs of strings.
+// Simplicity is favored over efficiency. Map lookups use linear search.
+//
+//
+// Code for Clark University CSCI 140 assignments.
+//
+// Author: John Magee
+// Date: 10/5/2017
+// Version 1.0
+//
+#ifndef MAP_H_INCLUDED
+#define MAP_H_INCLUDED
+
+#define STR_LENGTH 40
+
+typedef struct
+{
+  char *key;
+  char *value;
+} stringpair;
+
+typedef struct
+{
+  int mapSize;
+  int maxSize;
+  stringpair *pairs;
+} mapstruct;
+
+typedef mapstruct *map;
+
+// function definitions
+
+map createMap(int maxSize);
+void freeMap(map currentMap);
+int containsKey(map aMap, char *searchKey);
+char *lookupIndex(map aMap, int i);
+char *lookupKey(map aMap, char *searchKey);
+int insertKey(map aMap, char *key, char *value);
+
+#endif
